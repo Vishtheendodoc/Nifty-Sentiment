62aca820ca13eab3cdaf378b1cfc516057d8a420
Vishtheendodoc <dr.vishu.sp@gmail.com>
2025-10-10 12:11:03 +0530

    Update niftysentiment3.py

--- niftysentiment3.py
+++ niftysentiment3.py
@@ -46,7 +46,7 @@ st.set_page_config(page_title="Nifty Options IV Spike Dashboard", layout="wide")
 # 🔹 Dhan API Credentials (Replace with your own)
 # ====== Dhan API Config ======
 CLIENT_ID = '1100244268'
-ACCESS_TOKEN= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYwMDY1NTk0LCJpYXQiOjE3NTk5NzkxOTQsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.HL2o0kWnYdOkSHEM16wVR0yAyCYDKsX4CCNnF7iiEBYvVvEqzcEH3RJbQDxOgCGa-82lnxnzMsHBXaklJ5lCEQ'  # Replace with your Access Token
+ACCESS_TOKEN= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYwMTYxOTc5LCJpYXQiOjE3NjAwNzU1NzksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.XXxiYiOvkVPP2uBTRq4VK4Q0RCGPdJum2ISzlAaMqgJE6sMHpWq6WmnSx8cvUtIKcXLB_6cOD7VTEhdfcLRZoQ'  # Replace with your Access Token
 
 HEADERS = {
     'client-id': CLIENT_ID,
@@ -2288,5 +2288,6 @@ def ultimate_main_function():
 # 🔹 REPLACE YOUR MAIN EXECUTION
 if __name__ == "__main__":
     ultimate_main_function()
+
 
 
