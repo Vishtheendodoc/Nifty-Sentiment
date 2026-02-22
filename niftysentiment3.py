cc1665624a880d4e9f86aa70c82fbdccce7b8c71
Vishtheendodoc <dr.vishu.sp@gmail.com>
2026-02-22 22:20:48 +0530

    Update niftysentiment3.py

--- niftysentiment3.py
+++ niftysentiment3.py
@@ -142,7 +142,7 @@ load_session_state()
 # 🔹 Dhan API Credentials (Replace with your own)
 # ====== Dhan API Config ======
 CLIENT_ID = '1100244268'
-ACCESS_TOKEN= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzcxNjY5NDA3LCJpYXQiOjE3NzE1ODMwMDcsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.0vRrffj_fgmQZJetWD6nRsv0C8Gbj4tNjQjQDGqQp8Lr466WoyXbQz1Qn8EausiHAMSr3-UrGQQq4e9gyvktUw'  # Replace with your Access Token
+ACCESS_TOKEN= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzcxODY1MjQ3LCJpYXQiOjE3NzE3Nzg4NDcsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.HuwapCHUI31fyKHA-pxv7iAEwqyp4meONnMRKlY7kpF0hAJvDwfizMxteghav-ZrY496bGmpy-tIboYrZ6qWtQ'  # Replace with your Access Token
 
 HEADERS = {
     'client-id': CLIENT_ID,
@@ -2402,6 +2402,7 @@ def ultimate_main_function():
 # 🔹 REPLACE YOUR MAIN EXECUTION
 if __name__ == "__main__":
     ultimate_main_function()
+
 
 
 
